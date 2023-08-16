0d4f6015cf0488310f8cb70e3c524c73b1b4650d
josave <josave@odds.team>
2023-08-16 12:52:51 +0700

    สร้าง user service functions  สำหรับ CalculateAge

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using TRAINNING_EMPLOYEE_API.Models;
+using TRAINNING_EMPLOYEE_API.Services;
 
-namespace TRAINNING_EMPLOYEE_API.Controllers; //บอกว่าอยู่ภายใต้โฟลเดอร์อะไร
+namespace TRAINNING_EMPLOYEE_API.Controllers; //บอกว่าไฟล์นี้มาจากfolderอะไร
 
-[ApiController]         //ทางเข้า api
-[Route("api/users")]    //สร้างpath
-public class UserController: ControllerBase { //สรา้งclass
+[ApiController]
+[Route("api/users")] //เรียกใช้งาน api ที่pathไหน
+public class UserController : ControllerBase
+{
+    // https://localhost:5299/api/users
+    [HttpGet("")] //ใช้งาน method get ที่ path
+    public IActionResult GetAllUser()
+    {
 
-    [HttpGet("")]
-    public IActionResult GetAllUser(){
-        return Ok(new {
-            FirstName = "josave",
-            LastName="SEC",
-            YearOfBirth=1985,
-            Phone=00000000
-             //.net จะเปลี่ยนให้camelcsae
-        });
+        User user = new User();
+        UserService userService = new UserService();
+
+
+        user.FirstName = "Josave";
+        user.LastName = "SEC";
+        user.YearOfBirth = 1990;
+        user.Age = userService.CalculateAge(user.YearOfBirth);
+        return Ok(user);
     }
 }
---
+++ Services/UserService.cs
@@ -0,0 +1,6 @@
+namespace TRAINNING_EMPLOYEE_API.Services;
+public class UserService{
+    public int CalculateAge( int YearOfBirth){
+        return 2023 - YearOfBirth;
+    }
+}
