8cc9a3be048d72c87e4f04ff201d0b52e5d50dce
josave <josave@odds.team>
2023-08-16 14:14:21 +0700

    แก้ Models User

--- Models/User.cs
+++ Models/User.cs
@@ -1,18 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace TRAINNING_EMPLOYEE_API.Models;
 
 public class User{
+    public int UserId {get; set;}
     public string FirstName {get; set;} ="" ;
     public string LastName {get; set;} ="" ;
     public int YearOfBirth {get; set;}
     public int Age {get; set;}
 
-}
-
-public class UserDB : DbContext
-{
-    public UserDB(DbContextOptions<UserDB> options) : base (options) {}
-
-    public DbSet<User> Users {get; set;}
 }
